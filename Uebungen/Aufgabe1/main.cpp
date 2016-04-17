559a781594b2dc49c08357ab905caae0dd9bc651
Stefano Di Martino <stefano.d@posteo.de>
2016-04-17 19:15:05 +0200

    Ausgaben angepasst.

--- Uebungen/Aufgabe1/main.cpp
+++ Uebungen/Aufgabe1/main.cpp
@@ -179,7 +179,7 @@ void keyboard (unsigned char key, int x, int y)
             break;
         case '1':
             if (relativeStep <= 1) {
-                std::cout << "Linear Interpolation (LERP) with relative step: " << relativeStep << std::endl;
+                std::cout << "LERP with relative step: " << relativeStep << std::endl;
 
                 relativeStep += 0.1;
                 view.LERP_UP(startPoint, endPoint, relativeStep);
@@ -187,7 +187,7 @@ void keyboard (unsigned char key, int x, int y)
         break;
         case '2':
             if (relativeStep <= 1) {
-                std::cout << "Spherical Linear Interpolation (SLERP) with relative step: " << relativeStep << std::endl;
+                std::cout << "SLERP with relative step: " << relativeStep << std::endl;
 
                 relativeStep += 0.1;
                 view.SLERP_UP(startPoint, endPoint, relativeStep);
@@ -195,7 +195,7 @@ void keyboard (unsigned char key, int x, int y)
             break;
         case '3':
             if (relativeStep <= 1) {
-                std::cout << "Spherical Linear Interpolation (SLERP) with relative step: " << relativeStep << std::endl;
+                std::cout << "NSLERP with relative step: " << relativeStep << std::endl;
 
                 relativeStep += 0.1;
                 view.NSLERP_UP(startPoint, endPoint, relativeStep);
